a10f8b5a73850f24e3397ce5cde78e357fc11621
Bryan Kline <bryanjkline@gmail.com>
2018-07-23 18:49:37 -0700

    Add files via upload

---
+++ mergeTwoBinaryTrees.cpp
@@ -0,0 +1,63 @@
+/* 
+
+Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+
+Example 1:
+
+Input: 
+	Tree 1                     Tree 2                  
+          1                         2                             
+         / \                       / \                            
+        3   2                     1   3                        
+       /                           \   \                      
+      5                             4   7         
+         
+Output: 
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \ 
+	 5   4   7
+
+Note: The merging process must start from the root nodes of both trees. 
+
+*/
+
+class Solution 
+{
+    public:
+        TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) 
+        {
+            if(t1 == NULL && t2 == NULL)
+            {
+                return NULL;
+            }    
+            else
+            {
+                TreeNode* temp;
+                if(t1 != NULL && t2 == NULL)
+                {
+                    temp = new TreeNode(t1->val);
+                    temp->left = mergeTrees(t1->left, NULL);
+                    temp->right = mergeTrees(t1->right, NULL);
+                }   
+                else if(t1 == NULL && t2 != NULL)
+                {
+                    temp = new TreeNode(t2->val);
+                    temp->left = mergeTrees(NULL, t2->left);
+                    temp->right = mergeTrees(NULL, t2->right);
+                }   
+                else
+                {
+                    temp = new TreeNode(t1->val + t2->val);
+                    temp->left = mergeTrees(t1->left, t2->left);
+                    temp->right = mergeTrees(t1->right, t2->right);
+                }    
+                
+                return temp;
+            }    
+        }
+};
