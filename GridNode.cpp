ad997775bf5862e780e29b1464f30c1d67e47185
Bryan Kline <bryanjkline@gmail.com>
2018-10-09 18:25:39 -0700

    Update GridNode.cpp

--- GridNode.cpp
+++ GridNode.cpp
@@ -1,1 +1,27 @@
+class GridNode
+{
+  public:
+  
+    GridNode();
+    ~GridNode();
+  
+  private:
+  
+    GrideNode*** grid;
+};
 
+class Grid
+{
+  public:
+  
+    Grid();
+    Grid(int dimensions, int depth);
+    ~Grid();
+  
+  private:
+  
+    GridNode*** globalGrid;
+    int dimension;
+    int depth;
+  
+};
