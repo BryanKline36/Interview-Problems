49261740ce027ee08f31a3dce58990dfa6f64cd8
Bryan Kline <bryanjkline@gmail.com>
2018-10-10 19:45:03 -0700

    Update GridNode.cpp

--- GridNode.cpp
+++ GridNode.cpp
@@ -6,7 +6,16 @@ GridNode:: GridNode()
 
 GridNode:: ~GridNode()
 {
-
+  int first, second;
+  
+  for(first = 0; first < dimension; first++)
+  {
+    for(second = 0; second < dimension; second++)
+    {
+      delete [] grid[first][second];
+    }
+  }
+  
 } 
 
 Grid:: Grid()
